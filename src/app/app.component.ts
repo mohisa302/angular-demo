7f61980129eb026a2382227137c06dfbd34736eb
mohi <mohisadeghi302@gmail.com>
2024-09-15 18:46:33 +0330

    Add form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,30 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishItem';
 import { CommonModule } from '@angular/common'; // <-- Import CommonModule
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule], // <-- Add CommonModule here
+  imports: [RouterOutlet, CommonModule, FormsModule], // <-- Add CommonModule here
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   items: WishItem[] = [
-    // new WishItem('To Learn Angular'),
-    // new WishItem('Get Coffee', true),
-    // new WishItem('Learn Languages', false),
+    new WishItem('To Learn Angular'),
+    new WishItem('Get Coffee', true),
+    new WishItem('Learn Languages', false),
   ];
 
+  toggleItem(item: WishItem) {
+    item.isComplete = !item.isComplete;
+    console.log(item);
+  }
+  newWishText = '';
+  addNewWish() {
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText = '';
+  }
   title = 'angular-demo';
 }
