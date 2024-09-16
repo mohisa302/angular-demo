71e007a275499ae8427fd0ef4db00640f277e107
mohi <mohisadeghi302@gmail.com>
2024-09-16 11:33:38 +0330

    Add API

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,10 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { EventService } from '../shared/services/EventService'; // Import EventService
 import { routes } from './app.routes';
-
+import { provideHttpClient } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     EventService,
--- src/app/wish/wish.component.ts
+++ src/app/wish/wish.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../../shared/models/wishItem';
 import { CommonModule } from '@angular/common'; // <-- Import CommonModule
@@ -7,7 +7,7 @@ import { WishListComponent } from '../wish-list/wish-list.component';
 import { AddWishFormComponent } from '../add-wish-form/add-wish-form.component';
 import { WishFilterComponent } from '../wish-filter/wish-filter.component'; // <-- Import WishListComponent
 import { EventService } from '../../shared/services/EventService';
-
+import { WishService } from '../wish.service';
 @Component({
   selector: 'app-wish',
   standalone: true,
@@ -21,18 +21,18 @@ import { EventService } from '../../shared/services/EventService';
   templateUrl: './wish.component.html',
   styleUrl: './wish.component.css',
 })
-export class WishComponent {
-  constructor(events: EventService) {
+export class WishComponent implements OnInit {
+  ngOnInit(): void {
+    this.wishService.getWishes().subscribe((data: any) => {
+      this.items = data;
+    });
+  }
+  constructor(events: EventService, private wishService: WishService) {
     events.listen('removeWish', (wish: any) => {
       let index = this.items.indexOf(wish);
       this.items.splice(index, 1);
     });
   }
-  items: WishItem[] = [
-    new WishItem('To Learn Angular'),
-    new WishItem('Get Coffee', true),
-    new WishItem('Learn Languages', false),
-  ];
-
+  items: WishItem[] = []; // Initialize as an empty array
   filter: (item: WishItem) => boolean = () => true; // Default to show all items
 }
